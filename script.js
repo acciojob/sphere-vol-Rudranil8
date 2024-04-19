734f69bb98e2fd4dfed254038653cc3370549111
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-19 22:11:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function volume_sphere() {
-	let a= document.getElementById(radius);
-	return (4/3)*(22/7)*a*a*a;
+	// let a= document.getElementById(radius);
+	// return (4/3)*(22/7)*a*a*a;
+	let a = document.getElementById('radius').value;
+
+    if (isNaN(a) || a < 0) {
+        document.getElementById('volume').value = 'NaN';
+        return;
+    }
+    
+    let volume = (4/3) * Math.PI * Math.pow(a, 3);
+    
+    document.getElementById('volume').value = volume.toFixed(4);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
