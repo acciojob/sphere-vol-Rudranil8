5001776c942d8f06d59164354517f4695906809e
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-19 19:10:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function volume_sphere() {
-    //Write your code here
-  
+	let a= document.getElementById(radius);
+	return (4/3)*(22/7)*a*a*a;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
